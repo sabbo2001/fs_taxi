eedd5940d26f25bae7921f8990669ae01f00d26d
sabbo2001 <sabbo2001@hotmail.com>
2018-02-27 12:47:51 +0100

    Update en.lua

--- client/locales/en.lua
+++ client/locales/en.lua
@@ -1,9 +1,9 @@
 i18n.importData("en", {
 	taxi_contact = "Taxi call",
-	welcome_message = "Press ~g~F7~w~ or use ~g~/taxi~w~ to call a cab.",
+	welcome_message = "Use ~g~/taxi~w~ to call a cab.",
 	dispatch_message = "The taxi driver is on his way",
 	drivers_busy = "All our drivers are currently occupied.",
-	info_message = "Press ~INPUT_PICKUP~ to drive as a passenger or ~INPUT_ENTER~ to steal the vehicle.",
-	info_waypoint_message = "Select your destination on the map, then press ~INPUT_PICKUP~ to start.",
+	info_message = "Press ~INPUT_PICKUP~ to travel as a passenger.",
+	info_waypoint_message = "Select your destination on the map, then press ~INPUT_PICKUP~ to start. You will be charged for this trip.",
 	taxi_dispatch = "The taxi cab is on his way."
 })
